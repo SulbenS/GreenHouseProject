2a576855eff1a9aed24e18062003231bab088ffd
Petter J. Ytterdahl <petterjy@hotmail.no>
2024-12-02 06:51:20 +0100

    Concluding merge.

--- src/main/java/no/ntnu/server/ClientHandler.java
+++ src/main/java/no/ntnu/server/ClientHandler.java
@@ -5,14 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import no.ntnu.commands.ActuatorAddedInGui;
-import no.ntnu.commands.ActuatorCommand;
-import no.ntnu.commands.NodeIdentifier;
-import no.ntnu.commands.ActuatorIdentifier;
-import no.ntnu.commands.Data;
-import no.ntnu.commands.NodeCommand;
-import no.ntnu.commands.SensorIdentifier;
-import no.ntnu.commands.SensorReadingMessage;
+
+import no.ntnu.commands.*;
 import no.ntnu.tools.MessageHandler;
 
 /**
